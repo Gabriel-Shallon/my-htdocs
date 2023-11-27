4b5a7f9e9e01602720d4fb707543cf93d91e83d3
SHOGUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUN <shallonhatake@gmail.com>
2023-11-27 16:05:17 -0400

    a

--- app/index.php
+++ app/index.php
@@ -1,8 +1,8 @@
 <?php
 
-      require_once 'header.php';
-      require_once 'body.php';
-      require_once 'sidebar-footer.php';
-      require_once 'footer.php';
-      
+      require_once 'template/header.php';
+      require_once 'template/body.php';
+      require_once 'template/sidebar-footer.php';
+      require_once 'template/footer.php';
+
 ?>
