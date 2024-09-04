c9c30a0ab945eae7f85b4e726341f6ee108d1fb4
SHOGUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUN <shallonhatake@gmail.com>
2024-09-04 13:08:18 -0400

    i~j

--- Aulas Do Rafael - 3° ano/Vetor/atv02.php
+++ Aulas Do Rafael - 3° ano/Vetor/atv02.php
@@ -3,6 +3,11 @@
 
     $carros = array('gol', 'saveiro', 'celta');
 
+    echo '<br>';
+    echo '<pre>';
+    print_r($carros);
+    echo '<pre/>';
+
     foreach ($carros as $pos => $carro){
         if($carro == 'celta')
             $carros[$pos] = 'onix';
